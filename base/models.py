31e4bb8344d293de301c752cd6e9a8bae3f77d57
Aliaan <aliaan.azam02@gmail.com>
2024-02-02 13:50:45 +0100

    Implemented functionality for storing rooms in a sqllite database, and fetching them with queries.

--- base/models.py
+++ base/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+# creating models of tables in the Rooms
+
+class Room(models.Model):
+    #host = # The actual user that is connected
+    #topic =
+    name = models.CharField(max_length = 200) # Name of the room. 
+    description = models.TextField(null = True, blank = True) # Description of room. Makes sure the values can be left blank. 
+    #participants =  # Stores all the users active in a room
+    updated = models.DateTimeField(auto_now = True) # Takes a snapshot of anytime the table (model instance) is updated. Takes a timestamp every time room is updated.
+    created = models.DateTimeField(auto_now_add = True) # Takes a timestamp of when the instance was created.
+    
+    def __str__(self): # string representation of the room
+        return self.name
--- base/views.py
+++ base/views.py
@@ -1,21 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Room
 # Create your views here.
 
-rooms = [
-    {"id":1, "name":"Lets learn python!"},
-    {"id":2, "name":"Design with me!"},
-    {"id":3, "name":"Frontend developers!"},
-]
+# rooms = [
+#    {"id":1, "name":"Lets learn python!"},
+#    {"id":2, "name":"Design with me!"},
+#    {"id":3, "name":"Frontend developers!"},
+# ]
 
 def home(request): 
+    rooms = Room.objects.all() # query for rooms. Gives us all the rooms in the database. This is passed into the template.
     context = {"rooms" : rooms}
     return render(request, "base/home.html", context)
 
 def room(request, pk):
-    room = None
-    for i in rooms: 
-        if i["id"] == int(pk):
-            room = i
+    room = Room.objects.get(id=pk) # gets a single room from the database based on the unique id. 
     context = {"room" : room}
     return render(request, "base/room.html", context)
