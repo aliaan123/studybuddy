9e26b8eac03ae5db864fb1c3b82992ff12f0d90e
Aliaan <aliaan.azam02@gmail.com>
2024-02-02 16:04:44 +0100

    Added a topic class. Connected Message and Topic class to database

--- base/models.py
+++ base/models.py
@@ -5,9 +5,19 @@ from django.contrib.auth.models import User
 
 # creating models of tables in the Rooms
 
+
+# Topic class, represents the topic of the discussion.
+# Rooms are children of the topic class
+class Topic(models.Model):
+    name = models.CharField(max_length = 200) # Name of the topic
+    
+    def __str__(self): # string representation of the topic
+        return self.name
+
+# Room class, represents a chat room.
 class Room(models.Model):
-    #host = # The actual user that is connected
-    #topic =
+    host = models.ForeignKey(User, on_delete=models.SET_NULL, null = True) # The actual user that is connected. Relationship to a host. 
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null = True) # A topic can have multiple rooms, but a room can only have one topic. 
     name = models.CharField(max_length = 200) # Name of the room. 
     description = models.TextField(null = True, blank = True) # Description of room. Makes sure the values can be left blank. 
     #participants =  # Stores all the users active in a room
@@ -17,9 +27,15 @@ class Room(models.Model):
     def __str__(self): # string representation of the room
         return self.name
     
-# Message class. Each room is going to have multiple messages. One to many relationship in the database.  
+
+# Message class, represents a message sent by a user in a room. 
+# Has Room relationship and User relationship. Both are one to many relationships in the database. 
+# Each room is going to have multiple messages, messages has only one room.
+# Each user can have multiple messages, messages can only have one user. 
 class Message(models.Model):
-    #user =  # user that is sending the message
+    
+    user = models.ForeignKey(User, on_delete=models.CASCADE) # User that is sending the message. Import user model from the django library. 
+    
     # Foreign key used to establish the relationship in the database, and connect to the parent (Room).
     room = models.ForeignKey(Room, on_delete=models.CASCADE) # When the parent (room) is deleted, the children (messages) are also deleted.
     body = models.TextField() # the actual message
